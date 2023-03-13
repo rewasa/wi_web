ea11d25fcba5dd136d07abaa9adfb7f9d7689219
Renato Wasescha <renato.wasescha@agentselly.ch>
2023-03-13 15:16:56 +0100

    Test hedolight

--- app/routes/marktwertrechner.tsx
+++ app/routes/marktwertrechner.tsx
@@ -13,7 +13,7 @@ export default function Marktwertrechner() {
       iazi.hedolight('iazihedolightcontainer',
       '0AA0AB33DADB4CCEA69168F70F4916A4880BDA42F61442FB839A0006FD698A15',
       '{ "basePath": "https://app.iazi.ch/mod/hedolight"}')
-      }, 2000)
+      }, 5000)
     `;
     document.body.appendChild(script);
 
