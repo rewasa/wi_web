ff2f298bc7b1560f2a444f1fbc33e203aaa2631a
Renato Wasescha <renato.wasescha@agentselly.ch>
2023-03-13 21:58:59 +0100

    Fix

--- app/routes/marktwertrechner.tsx
+++ app/routes/marktwertrechner.tsx
@@ -50,7 +50,7 @@ export default function Marktwertrechner() {
             {loaderValue}%
           </div>
 
-          <div className={clsx({ visible: loaderValue < 100 })}></div>
+          <div className={clsx({ visible: loaderValue > 100 })}></div>
         </div>
       </Container>
     </Layout>
