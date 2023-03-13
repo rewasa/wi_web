e0e6303c5826d6fef0a9fc1dc072a82659dd3598
Renato Wasescha <renato.wasescha@agentselly.ch>
2023-03-13 21:45:37 +0100

    show loader for hedolight

--- app/routes/marktwertrechner.tsx
+++ app/routes/marktwertrechner.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { Container } from "~/components/Container/Container";
 import { Heading } from "~/components/Heading/Heading";
 import { Layout } from "~/components/Layout/Layout";
 
 export default function Marktwertrechner() {
+  const [loaderValue, setLoaderValue] = useState(0);
+
   useEffect(() => {
     const scriptIazi = document.createElement("script");
     scriptIazi.src = "https://app.iazi.ch/mod/hedolight/interface.js";
@@ -13,24 +17,42 @@ export default function Marktwertrechner() {
       iazi.hedolight('iazihedolightcontainer',
       '0AA0AB33DADB4CCEA69168F70F4916A4880BDA42F61442FB839A0006FD698A15',
       '{ "basePath": "https://app.iazi.ch/mod/hedolight"}')
-      }, 5000)
+    }, 5000);
     `;
     document.body.appendChild(script);
+
+    const interval = setInterval(() => {
+      setLoaderValue((loaderValue) => loaderValue + 1);
+    }, 60);
+
+    if (loaderValue === 100) {
+      clearInterval(interval);
+    }
 
     return () => {
       document.body.removeChild(script);
       document.body.removeChild(scriptIazi);
+      clearInterval(interval);
     };
   }, []);
 
   return (
     <Layout>
       <Heading isMain title="Marktwertrechner" />
-      <div
-        id="iazihedolightcontainer"
-        // on-load-language="de"
-        // className="h-min-screen w-min-screen"
-      ></div>
+      <Container>
+        <div className="flex min-h-[400px] items-center justify-center">
+          <div
+            className={clsx("radial-progress text-secondary", {
+              invisible: loaderValue > 100,
+            })}
+            style={{ "--value": loaderValue }}
+          >
+            {loaderValue}%
+          </div>
+
+          <div className={clsx({ visible: loaderValue < 100 })}></div>
+        </div>
+      </Container>
     </Layout>
   );
 }
