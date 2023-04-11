cc741380071b19d32ac9a730d29366b720628809
Renato Wasescha <renato.wasescha@agentselly.ch>
2023-04-11 16:17:09 +0200

    Add sitemmap and robot

---
+++ app/routes/robots[.]txt.tsx
@@ -0,0 +1,18 @@
+export const loader = () => {
+  const robotText = `
+    User-agent: Googlebot
+    Disallow: /nogooglebot/
+
+    User-agent: *
+    Allow: /
+
+    Sitemap: https://wasescha.immobilien/sitemap.xml
+    `;
+
+  return new Response(robotText, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+};
