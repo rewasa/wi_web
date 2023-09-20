7936a76e49f76611215f18ca1f827c2f92d9ab37
Renato Wasescha <renato.wasescha@agentselly.ch>
2023-09-20 10:41:58 +0200

    Add text

--- app/routes/marktwertrechner.tsx
+++ app/routes/marktwertrechner.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+
 import { Container } from "~/components/Container/Container";
 import { Heading } from "~/components/Heading/Heading";
 import { Layout } from "~/components/Layout/Layout";
-import useHedolight from "~/hooks/useHedolight";
 import type { Page } from "~/page.server";
 import { metaData } from "~/utils/metaData";
+import useHedolight from "~/hooks/useHedolight";
 
 const scripts = () => {
   return [
@@ -18,8 +19,10 @@ export const handle = { scripts };
 
 export const meta = () => {
   const marktwertrechnerMeta = {
-    title: "Marktwertrechner",
-    description: "Marktwertrechner",
+    title:
+      "Du möchtest den Marktwert deiner Immobilie kennen? | Marktwertrechner",
+    description:
+      "Nutze unser Online-Tool und erhalte in wenigen Minuten eine kostenlose und unverbindliche Immobilienbewertung.",
     slug: "/marktwertrechner",
     ogImage: {
       id: "bad74d54-58ee-461c-8768-ad7cfda57bb1", // Hero Image
@@ -43,7 +46,15 @@ export default function Marktwertrechner() {
 
   return (
     <Layout>
-      <Heading isMain title="Marktwertrechner" />
+      <Heading title="Du möchtest den Marktwert deiner Immobilie kennen?" />
+      <div className="px-4 xl:pl-36 xl:pr-20 text-xl">
+        <p className="pb-2">Wir haben die Lösung für dich!</p>
+        <p className="pb-2">
+          Nutze unser Online-Tool und erhalte in wenigen Minuten eine kostenlose
+          und unverbindliche Immobilienbewertung.
+        </p>
+      </div>
+
       <Container>
         <div className="flex min-h-[400px] items-center justify-center">
           {loaderValue < 100 ? (
